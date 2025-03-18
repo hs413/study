b9a4b86ccaf94ce93d5d2e729fa086cf4fe9f7a7
rtg <rtg2541@gmail.com>
2025-03-18 22:34:10 +0900

    FP: 액션,계산,데이터 구분 예시2

--- programming/functional-programming/coupon-my.js
+++ programming/functional-programming/coupon-my.js
@@ -10,8 +10,12 @@ const bestSubscribers = findBestSubscribers(subscribers);
 // 4. 쿠폰 목록에서 높은 등급의 쿠폰을 뽑아낸다. (계산)
 const bestCoupon = findBestCoupons(coupons);
 
-// 5. 3번에서 뽑아낸 구독자에게 4번에서 뽑아낸 쿠폰을 발송한다. (액션)
-bestSubscribers.forEach(subscriber => sendMail(subscriber, bestCoupon))
+// 5번 과정 분리
+// 5-1. 발송 메일 목록을 만든다. (계산)
+const mailList = createMailList(bestSubscribers, bestCoupon);
+
+// 5-2. 메일을 발송한다. (액션)
+sendMail(mailList);
 
 
 // DB에서 구독자 목록을 가져온다.
@@ -33,6 +37,7 @@ function fetchCoupons() {
   ]
 }
 
+// 좋은 구독자를 뽑아낸다.
 function findBestSubscribers(subscribers) {
   const bestSubscribers = [];
   for (let i = 0; i < subscribers.length; i++) {
@@ -45,6 +50,7 @@ function findBestSubscribers(subscribers) {
   // return subscribers.filter(subscriber => subscriber.count >= 10);
 }
 
+// 좋은 쿠폰을 뽑아낸다.
 function findBestCoupons(coupons) {
   for (let i = 0; i < coupons.length; i++) {
     const coupon = coupons[i];
@@ -55,11 +61,28 @@ function findBestCoupons(coupons) {
   // return coupons.find(coupon => coupon.rank === 'best');
 }
 
-function sendMail(subscriber, coupon) {
-  const content = {
-    to: subscriber.email,
-    coupon: coupon,
+// 메일 목록을 만든다.
+function createMailList(subscribers, coupon) {
+  const mailList = [];
+  for (let i = 0; i < subscribers.length; i++) {
+    mailList.push({
+      subscribers: subscribers[i],
+      coupon: coupon
+    })
   }
-  // 메일을 보낸다고 가정
-  console.log(content)
+
+  return mailList;
+
+  // return subscribers.map(subscriber => ({
+  //   subscriber,
+  //   coupon,
+  // }))
+}
+
+// 메일을 보낸다.
+function sendMail(mailList) {
+  for (let i = 0; i < mailList.length; i++) {
+    // 메일을 보낸다고 가정
+    console.log(mailList[i])
+  }
 }
