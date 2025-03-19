c315230673ff9766fb58d2dbab7aa97626f11960
rtg <rtg2541@gmail.com>
2025-03-19 12:53:41 +0900

    FP: 액션에서 계산 빼내기1

---
+++ programming/functional-programming/ch04/megamart.js
@@ -0,0 +1,44 @@
+var shopping_cart = []; // 액션 - 변경 가능
+var shopping_cart_total = 0; // 액션 - 변경 가능
+
+function add_item_to_cart(name, price) {
+  // 액션 - 전역변수를 변경
+  shopping_cart.push({
+    name,
+    price,
+  })
+  calc_cart_total();
+}
+
+function calc_cart_total() {
+  // 액션 - 전역변수 변경
+  shopping_cart_total = 0;
+  for (let i = 0; i < shopping_cart.length; i++) {
+    var item = shopping_cart[i];
+    shopping_cart_total += item.price;
+  }
+
+  set_cart_total_dom();
+  update_shipping_icons();
+  set_tax_dom;
+}
+
+function update_shipping_icons() {
+  // 액션 - DOM에서 읽음
+  var buy_buttons = get_buy_buttons_dom();
+  for (let i = 0; i < buy_buttons.length; i++) {
+    var button = buy_buttons[i];
+    var item = button.item;
+    // 액션 - DOM을 변경
+    if(item.price + shopping_cart_total >= 20) {
+      button.show_free_shipping_icon();
+    } else {
+      button.hide_free_shipping_icon();
+    }
+  }
+}
+
+function update_tax_dom() {
+  // 액션 - DOM을 변경
+  set_tax_dom(shopping_cart_total * 0.10);
+}
