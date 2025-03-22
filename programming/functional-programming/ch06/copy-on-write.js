9b63ed357f132d9e8161ca6a6207eaefd0008200
rtg <rtg2541@gmail.com>
2025-03-22 22:12:31 +0900

    FP: 카피 온 라이트2

--- programming/functional-programming/ch06/copy-on-write.js
+++ programming/functional-programming/ch06/copy-on-write.js
@@ -1,5 +1,5 @@
-// 초기 버전
-function removeItemByName0(cart, name) {
+// v0
+function removeItemByNameV0(cart, name) {
   let idx = null;
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].name === name) {
@@ -12,12 +12,12 @@ function removeItemByName0(cart, name) {
   }
 }
 
-function deleteHandler0(name) {
+function deleteHandlerV0(name) {
   removeItemByName0(shoppingCart, name);
 }
 
-// --- 카피 온 라이트 버전
-function removeItemByName(cart, name) {
+// v1 카피 온 라이트 적용
+function removeItemByNameV1(cart, name) {
   let newCart = cart.slice();
   let idx = null;
 
@@ -28,11 +28,38 @@ function removeItemByName(cart, name) {
   }
 
   if (idx !== null) {
-    newCart.splice(idx, 1);
+    removeItemV1(newCart, idx, 1);
   }
   return newCart
 }
 
-function deleteHandler(name) {
+function deleteHandlerV1(name) {
   shoppingCart = removeItemByName(shoppingCart, name);
+}
+
+function removeItemV1(array, idx, count) {
+  array.splice(idx, count)
+}
+
+// v2
+function removeItemV2(array, idx, count) {
+  const copy = array.slice();
+  copy.splice(idx, count);
+  return copy;
+}
+
+function removeItemByNameV2(cart, name) {
+  let idx = null;
+
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].name === name) {
+      idx = i
+    }
+  }
+
+  if (idx !== null) {
+    return removeItemV2(cart, idx, 1);
+  }
+
+  return cart;
 }
